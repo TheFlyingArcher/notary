b9ff9a6bcee9f0ba7246b7e95ca65d854ae793c0
bcline <brandon.cline.619@gmail.com>
2026-04-23 16:30:22 -0700

    Package upgrading

--- Notary.AppHost/Program.cs
+++ Notary.AppHost/Program.cs
@@ -2,13 +2,7 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var mongo = builder.AddMongoDB("notary-mongo", 27017)
-    .WithMongoExpress()
-    .WithLifetime(ContainerLifetime.Persistent);
-var mongodb = mongo.AddDatabase("notary");
-
 builder.AddProject<Notary_Web>("portal")
-    .WithHttpsHealthCheck()
-    .WaitFor(mongodb);
+    .WithHttpsHealthCheck();
 
 builder.Build().Run();
